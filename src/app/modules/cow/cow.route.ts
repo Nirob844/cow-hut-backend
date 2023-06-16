0042cf5f1e547f093c3437220f4fd778bdc858cc
Nirob Hasan <nirob427417@gmail.com>
2023-06-16 21:43:18 +0600

    update delite

--- src/app/modules/cow/cow.route.ts
+++ src/app/modules/cow/cow.route.ts
@@ -3,6 +3,9 @@ import { CowController } from './cow.controller';
 
 const router = express.Router();
 
+router.get('/:id', CowController.getSingleCow);
+router.patch('/:id', CowController.updateCow);
+router.delete('/:id', CowController.deleteCow);
 router.post('/create-cow', CowController.createCow);
 router.get('/', CowController.getAllCows);
 
